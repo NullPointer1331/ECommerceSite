f8bff2b94e933b079e766bd9cf5a6e18eb7351a1
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-07-29 12:20:58 -0700

    set up connection string

---
+++ ECommerceSite/Data/PetStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ECommerceSite.Data
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetStoreContext(DbContextOptions<PetStoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
