0bc2c36459f86de526e1a7164401b5619370e978
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-08-28 14:59:43 -0700

    Added cart button and controller

---
+++ ECommerceSite/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using ECommerceSite.Data;
+using ECommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly PetStoreContext _context;
+
+        public CartController(PetStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Add(int id)
+        {
+            Product? product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                TempData["Message"] = "Product not found";
+            }
+            else
+            {
+                TempData["Message"] = $"Product {product.Name} added to cart";
+            }
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
