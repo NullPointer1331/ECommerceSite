1a1570869ba92f8a92bdddfa191ddbc5dd80dda3
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-08-28 15:46:32 -0700

    Implemented Cart

--- ECommerceSite/Controllers/CartController.cs
+++ ECommerceSite/Controllers/CartController.cs
@@ -1,11 +1,14 @@
 ﻿using ECommerceSite.Data;
 using ECommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
 namespace ECommerceSite.Controllers
 {
     public class CartController : Controller
     {
+        private const string Cart = "Cart";
         private readonly PetStoreContext _context;
 
         public CartController(PetStoreContext context)
@@ -22,9 +25,65 @@ namespace ECommerceSite.Controllers
             }
             else
             {
+                CartItemViewModel cartItem = new CartItemViewModel
+                {
+                    ProductId = product.ProductId,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Quantity = 1
+                };
+                List<CartItemViewModel> cartItems = GetCartData();
+                if (cartItems.Any(c => c.ProductId == cartItem.ProductId))
+                {
+                    CartItemViewModel existingItem = cartItems.First(c => c.ProductId == cartItem.ProductId);
+                    existingItem.Quantity++;
+                }
+                else
+                {
+                    cartItems.Add(cartItem);
+                }
+
+                SetCartCookie(cartItems);
                 TempData["Message"] = $"Product {product.Name} added to cart";
             }
             return RedirectToAction("Index", "Product");
+        }
+
+
+        private List<CartItemViewModel> GetCartData()
+        {
+            string cookie = HttpContext.Request.Cookies[Cart];
+            if (cookie.IsNullOrEmpty())
+            {
+                return new List<CartItemViewModel>();
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<List<CartItemViewModel>>(cookie);
+            }
+        }
+
+        public IActionResult Summary()
+        {
+            List<CartItemViewModel> cartItems = GetCartData();
+            return View(cartItems);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            List<CartItemViewModel> cartItems = GetCartData();
+            CartItemViewModel? item = cartItems.FirstOrDefault(c => c.ProductId == id);
+            cartItems.Remove(item);
+            SetCartCookie(cartItems);
+            return RedirectToAction("Summary");
+        }
+
+        private void SetCartCookie(List<CartItemViewModel> cartItems)
+        {
+            HttpContext.Response.Cookies.Append(Cart, JsonConvert.SerializeObject(cartItems), new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1)
+            });
         }
     }
 }
--- ECommerceSite/Models/Product.cs
+++ ECommerceSite/Models/Product.cs
@@ -46,4 +46,12 @@ namespace ECommerceSite.Models
             Price = price;
         }
     }
+
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
 }
